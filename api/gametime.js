12f5f567a4e644b79a7faf44a7dc105a8c408b89
Daniel Stansberry <stansberry2295@gmail.com>
2017-03-10 21:12:46 -0800

    included console logs and declaration for user/team arrays

--- api/gametime.js
+++ api/gametime.js
@@ -7,28 +7,28 @@ const {
     DailyGamesRef,
     GameIDRef
 } = require('../utils/firebase');
+const moment = require('moment');
 
 var ParsePostTitle = (
     (title, subreddit) => {
         var currentDate = moment().format("MM-DD-YYYY");
         if (subreddit == "nbastreams") {
             var indexColon = title.indexOf(':'),
-                indexAt = title.indexOf('vs'),
+                indexAt = title.indexOf('@'),
                 indexLeftParen = title.indexOf('('),
                 indexET = title.indexOf('ET'),
                 gameTime = currentDate + " " + title.substring(indexLeftParen + 1, indexET - 1),
                 teamA = title.substring(indexColon + 2, indexAt - 1).toLowerCase(),
-                teamB = title.substring(indexAt + 3, indexLeftParen - 1).toLowerCase();
-        } else if (subreddit == "nhlstreams") {
+                teamB = title.substring(indexAt + 2, indexLeftParen - 1).toLowerCase();
+                console.log(`teamA is ${teamA}, teamB is ${teamB}`);
+        } else if (subreddit == "nhlstreams" || subreddit == "mlbstreams") {
             var indexColon = title.indexOf(':'),
                 indexAt = title.indexOf('at '),
                 indexPM = title.indexOf('PM'),
                 gameTime = currentDate + " " +
                 title.substring(indexPM - 6, indexPM + 2);
-            teamA = title.substring(indexColon + 2, indexAt - 1).toLowerCase(),
+                teamA = title.substring(indexColon + 2, indexAt - 1).toLowerCase(),
                 teamB = title.substring(indexAt + 3, indexPM - 6).toLowerCase();
-        } else if (subreddit == "mlbstreams") {
-            console.log("parse mlbstreams");
         }
         if (gameTime[0] == " ") {
             gameTime = gameTime.substring(1);
@@ -40,21 +40,23 @@ var ParsePostTitle = (
     }
 );
 
-var SetDailyGames = ((snapshot, post) => {
+async function SetDailyGames(snapshot, post, subreddit) {
     var containsGameAlready = false;
-    snapshot.forEach(childSnapshot => {
+    await snapshot.forEach(childSnapshot => {
+        console.log(`childSnapshot.key is ${childSnapshot.key}`);
         if (childSnapshot.key == post.id) {
             containsGameAlready = true;
         }
     });
+
     if (!containsGameAlready) {
-        var parsedPost = ParsePostTitle(post.title, subreddit);
-        DailyGamesRef.child(subreddit).child(post.id).set({
-            "gametime": parsedPost.GameTime,
-            "teams": [parsedPost.Teams[0], parsedPost.Teams[1]]
-        });
+      var parsedPost = ParsePostTitle(post.title, subreddit);
+      DailyGamesRef.child(subreddit).child(post.id).set({
+        "gametime": parsedPost.GameTime,
+        "teams": [parsedPost.Teams[0], parsedPost.Teams[1]]
+      });
     }
-});
+};
 
 module.exports.RetrieveGameTimes = (
     (subreddit, gameIDs) => {
@@ -64,9 +66,9 @@ module.exports.RetrieveGameTimes = (
             .then((posts) => {
                 gameIDs.then(gameIds => {
                     for (let post of posts) {
-                        if ((gameIds.indexOf(post.id) == -1) && (post.title.toLowerCase().includes("game thread")) && (post.author.name.toLowerCase() == "nbastreamsbotv2")) {
+                        if ((gameIds.indexOf(post.id) == -1) && (post.title.toLowerCase().includes("game thread"))) {
                             DailyGamesRef.child(subreddit).once("value").then(snapshot => {
-                                SetDailyGames(snapshot, post);
+                                SetDailyGames(snapshot, post, subreddit);
                             });
                             GameIDRef.push(post.id);
                         }
